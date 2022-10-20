8dc56d3a19413076a4c2cb8bd3653b55c1ee9ab0
Rohit <Rohit@spotblhol0128.awsspoton.com>
2022-10-20 22:01:01 +0530

    Welcome to snake and ladder program

---
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and ladder program");
+    }
+}
